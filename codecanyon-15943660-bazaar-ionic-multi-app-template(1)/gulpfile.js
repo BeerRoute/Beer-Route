9296f88b667e5fd18b6d6d9dd77207aa79dd9e55
Unknown <miguel.velez9@upr.edu>
2016-10-12 12:35:30 -0400

    Removing old UI folder

--- codecanyon-15943660-bazaar-ionic-multi-app-template(1)/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-
-var requireDir = require('require-dir');
-requireDir('./gulp-tasks');
-
-gulp.task('serve:before', ['default','watch']);
-
-gulp.task('default', ['sass', 'templatecache']);
