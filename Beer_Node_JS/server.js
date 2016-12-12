223852bfcedda4bf0c00732cf061601124746c54
GreenThunder42 <jose.rodriguez42@upr.edu>
2016-12-12 18:47:04 -0400

    Changing type

--- Beer_Node_JS/server.js
+++ Beer_Node_JS/server.js
@@ -189,7 +189,7 @@ var response = [];
        		auth: {user: 'beer.route2016@gmail.com', // Your email id
             	pass: 'Beer_Craft2016'} // Your password
     		});
-		mailOptions.subject = 'Welcome to Beer-Route';
+		mailOptions.subject = 'Welcome to Beer Route';
 		mailOptions.text = 'Hello. Thank you for creating an account in Beer Route. Please confirm your account by logging in \n\n' + 'http://localhost:8100/#/intro/auth-login';
 		mailOptions.to = req.query.email
 		transporter.sendMail(mailOptions, function(error, info){
